67fa8878c7087a78b0959982178664dd9ea0ed06
Cu Le Huy <lcu@usc.edu>
2016-04-27 14:48:24 -0700

    Initial Commit

---
+++ bThere/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bThere
+//
+//  Created by Le Huy Cu on 4/27/16.
+//  Copyright © 2016 Le Huy Cu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bThere/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bThere
+//
+//  Created by Le Huy Cu on 4/27/16.
+//  Copyright © 2016 Le Huy Cu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
